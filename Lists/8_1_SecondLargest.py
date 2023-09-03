9a162648a6f0310282d30e923ef3a77870488817
MangeshM07 <mangesh.mankar04@gmail.com>
2023-09-03 21:21:54 +0530

    git commit -m "Lists"

---
+++ Lists/8_1_SecondLargest.py
@@ -0,0 +1,20 @@
+def secondLargest(arr):
+    if len(arr) < 2:
+        return "Number of elements in the list should be atleast 2"
+
+    largest = second_largest = float('-inf')
+    for num in arr:
+        if num > largest:
+            second_largest = largest
+            largest = num
+        elif num > second_largest and num != largest:
+            second_largest = num
+
+    if second_largest == float('-inf'):
+        return "No second largest element found"
+    else:
+        return second_largest
+
+my_list = [12, 45, 2, 41, 31, 10, 8]
+result = secondLargest(my_list)
+print("Second largest element:", result)

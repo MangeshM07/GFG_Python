6d7bf88cc07a0b5e3b9c03be8ac6b5213f360c39
MangeshM07 <mangesh.mankar04@gmail.com>
2024-01-06 09:12:03 +0530

    finding peak element using binary search

--- peakElement.py
+++ peakElement.py
@@ -12,6 +12,35 @@ def peakElement(array,n):
         if array[i-1] < array[i]  > array[i+1]:
             return i
 
+
+def peakElementUsingBinarySearch(array,n):
+    if n == 0:
+        return None
+    elif n==1:
+        return 0
+    elif n==2:
+        if array[0] > array[1]:
+            return 0
+        else:
+            return 1
+
+    low = 0
+    high = n-1
+
+    while low <= high:
+        mid = (low+high)//2
+        if array[mid-1] <= array[mid] and array[mid] >= array[mid+1]:
+            return mid
+        elif mid != 0 and array[mid] < array[mid-1]:
+            high = mid -1
+        elif array[mid] < array[mid+1] and mid+1 == n-1 :
+            return mid+1
+        else:
+            low = mid + 1
+
+    return -1
+
 array = [3,4,2]
-array1=[1,2,3]
-print(peakElement(array1,3))
+array1=[1,2,3,4]
+array2 = [497, 850, 64, 479, 511, 958]
+print(peakElementUsingBinarySearch(array2,6))

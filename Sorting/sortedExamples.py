fc42a3d7885aaca4e828c6d0466268649a6ead7e
MangeshM07 <mangesh.mankar04@gmail.com>
2024-01-15 23:04:20 +0530

    sorted method

---
+++ Sorting/sortedExamples.py
@@ -0,0 +1,22 @@
+# Sorted() in Python
+# ===================
+#
+#
+# --> Works for any iterables(List, Tuple, Dictionary, String, Set, etc)
+# --> Returns a list with sorted items
+# --> parameters like reverse and key work same way as sort()
+#
+# */
+from builtins import print
+
+l = [10, 20, 14]
+ls = sorted(l)
+
+print(l)
+print(ls)
+
+l = [10, -15, -2, 1]
+ls = sorted(l, key = abs, reverse=True)
+
+nls = sorted(l, reverse=False)
+print(nls)

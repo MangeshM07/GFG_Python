5915eb41032efd8b7d8ce2c512cf7053123e249f
MangeshM07 <mangesh.mankar04@gmail.com>
2024-02-22 19:11:32 +0530

    Hoares partition

---
+++ Sorting/hoaresPartition.py
@@ -0,0 +1,27 @@
+def hoaresPartition(arr, l , h):
+    pivot = arr[l]
+    i = l-1
+    j = h+1
+
+    while True:
+        i = i+1
+        while arr[i] < pivot:
+            i = i+1
+
+        j = j-1
+        while arr[j] > pivot:
+            j = j-1
+
+        if i >= j:
+            return j
+
+        arr[i], arr[j] = arr[j], arr[i]
+
+
+arr = [5,3,8,4,2,7,1,10]
+l = 0
+h = 7
+
+hoaresPartition(arr, l ,h)
+
+print(arr)

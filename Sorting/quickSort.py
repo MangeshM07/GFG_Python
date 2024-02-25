feffb8eea2fd3a8f0d4e87543aab16d52ed705bd
MangeshM07 <mangesh.mankar04@gmail.com>
2024-02-25 19:25:53 +0530

    quick sort

---
+++ Sorting/quickSort.py
@@ -0,0 +1,13 @@
+'''
+Introduction:
+==============
+1) Divide and Conquer Algorithm
+2) Worst case time : O(n^2)
+3) Despite this it is considered faster because of the following:
+    a) In-Place
+    b) Cache- friendly
+    c) Average case is O(nlog(n))
+    d) Tail recursive
+4) Partition is key function(naive, Lomuto, Hoars)
+
+'''

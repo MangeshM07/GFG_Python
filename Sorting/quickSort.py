ea4ce134236157accdb47f6d7505f2b1656e9004
MangeshM07 <mangesh.mankar04@gmail.com>
2024-02-27 21:52:00 +0530

    quick sort

--- Sorting/quickSort.py
+++ Sorting/quickSort.py
@@ -11,3 +11,26 @@ Introduction:
 4) Partition is key function(naive, Lomuto, Hoars)
 
 '''
+def lomutoPartition(arr, l, h):
+    pivot = arr[h]
+    i = l - 1
+    for j in range(l, h):
+        if arr[j] <= pivot:
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[h] = arr[h], arr[i + 1]
+    return i + 1
+
+def qsort(arr, l ,h):
+    if l < h:
+        p = lomutoPartition(arr, l, h)
+        qsort(arr, l , p-1)
+        qsort(arr, p+1, h)
+
+
+arr = [10, 80, 30, 90, 40, 50, 70]
+l = 0
+h = 6
+
+qsort(arr, l, h)
+print(arr)
